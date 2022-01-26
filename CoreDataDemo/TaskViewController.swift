79516034f0bc49d42d8f8245a1f195e05fff0fff
Elisei Mishchenko <esm.mail@icloud.com>
2022-01-26 17:18:04 +0300

    Creating a text field placing on the screen and setting parameters two buttons were also created "Save Task" and "Cancel" the parameters and placement o n the screen were set for them

--- CoreDataDemo/TaskViewController.swift
+++ CoreDataDemo/TaskViewController.swift
@@ -8,21 +8,92 @@
 import UIKit
 
 class TaskViewController: UIViewController {
+    
+    private lazy var taskTextField: UITextField = {
+        let textField = UITextField()
+        
+        textField.placeholder = "New Task"
+        textField.borderStyle = .roundedRect
+        return textField
+    }()
+    
+    private lazy var saveButton: UIButton = {
+        createButton(
+            withTitle: "Save Task",
+            andColor: UIColor(
+                red: 21/255,
+                green: 101/255,
+                blue: 192/255,
+                alpha: 1
+            ),
+            action: UIAction { _ in
+                self.dismiss(animated: true)
+            })
+        
+    }()
+    
+    private lazy var canselButton: UIButton = {
+        createButton(
+            withTitle: "Cancel",
+            andColor: UIColor(
+                red: 234/255,
+                green: 104/255,
+                blue: 91/255,
+                alpha: 1
+            ),
+            action: UIAction { _ in
+                self.dismiss(animated: true)
+            })
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
+        setup(subviews: taskTextField, saveButton, canselButton)
+        setConstrains()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func setup(subviews: UIView...) {
+        subviews.forEach { subview in
+            view.addSubview(subview)
+        }
     }
-    */
-
+    
+    private func setConstrains() {
+        taskTextField.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            taskTextField.topAnchor.constraint(equalTo: view.topAnchor, constant: 80),
+            taskTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 40),
+            taskTextField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -40)
+        ])
+        
+        saveButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            saveButton.topAnchor.constraint(equalTo: taskTextField.bottomAnchor, constant: 20),
+            saveButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 40),
+            saveButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -40)
+        ])
+        
+        canselButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            canselButton.topAnchor.constraint(equalTo: saveButton.bottomAnchor, constant: 20),
+            canselButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 40),
+            canselButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -40)
+        ])
+    }
+    
+    private func createButton(withTitle title: String, andColor color: UIColor, action: UIAction) -> UIButton {
+        var buttonConfiguration = UIButton.Configuration.filled()
+        buttonConfiguration.baseBackgroundColor = color
+        
+        var attributes = AttributeContainer()
+        
+        attributes.font = UIFont.boldSystemFont(ofSize: 18)
+        buttonConfiguration.attributedTitle = AttributedString(title, attributes: attributes)
+        
+        return UIButton(configuration: buttonConfiguration, primaryAction: action)
+    }
 }
