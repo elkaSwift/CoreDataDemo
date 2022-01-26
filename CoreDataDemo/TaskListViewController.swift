b3ec03cf55e1ec4a42b9ab4c8fcb5dc9dd91f9e3
Elisei Mishchenko <esm.mail@icloud.com>
2022-01-26 19:03:43 +0300

    An AlertController has been created that replaces the work of the TaskViewController file

--- CoreDataDemo/TaskListViewController.swift
+++ CoreDataDemo/TaskListViewController.swift
@@ -58,9 +58,7 @@ class TaskListViewController: UITableViewController {
     }
     
     @objc private func addNewTask() {
-        let newTaskVC = TaskViewController()
-        newTaskVC.modalPresentationStyle = .fullScreen 
-        present(newTaskVC, animated: true)
+        showAlert(with: "New Task", and: "What do you want to do?")
     }
     
     private func fetchData() {
@@ -73,6 +71,37 @@ class TaskListViewController: UITableViewController {
         }
     }
     
+    private func showAlert(with title: String, and message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let saveAction = UIAlertAction(title: "Save", style: .default) { _ in
+            guard let task = alert.textFields?.first?.text, !task.isEmpty else { return }
+            self.save(task)
+        }
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .destructive)
+        
+        alert.addAction(saveAction)
+        alert.addAction(cancelAction)
+        alert.addTextField { textField in
+            textField.placeholder = "New Task"
+        }
+        present(alert, animated: true)
+    }
+    private func save(_ taskName: String) {
+        
+        let task = Task(context: context)
+        task.name = taskName
+        taskList.append(task)
+        
+        let cellIndex = IndexPath(row: taskList.count - 1, section: 0)
+        tableView.insertRows(at: [cellIndex], with: .automatic)
+        
+        do {
+            try context.save()
+        } catch let error {
+            print(error)
+        }
+    }
 
 }
 
@@ -90,4 +119,5 @@ extension TaskListViewController {
         cell.contentConfiguration = content
         return cell
     }
+    
 }
